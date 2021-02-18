f9e5c82531193fc706b289360997d4b9ec183224
Ronildo Souza <ronildo_12@outlook.com>
2021-02-18 16:15:11 -0300

    Entity OrdersProducts

---
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -0,0 +1,40 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  Entity,
+  ManyToOne,
+  JoinColumn,
+  Column,
+} from 'typeorm';
+import Order from './Order';
+
+import Product from '@modules/products/typeorm/entities/Product';
+
+@Entity('orders_products')
+class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default OrdersProducts;
